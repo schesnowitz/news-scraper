dc89e9aa57539333e8ddd56027d9b956d1ebd807
Steve Chesnowitz <steve@chesnowitz.com>
2018-02-10 00:08:12 -0500

    more precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,10 +2,16 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( material-design-iconic-font/dist/css/material-design-iconic-font.min.css )
+Rails.application.config.assets.precompile += %w( animate.css/animate.min.css )
+Rails.application.config.assets.precompile += %w( jquery.scrollbar/jquery.scrollbar.css )
+Rails.application.config.assets.precompile += %w( fullcalendar/dist/fullcalendar.min.css )
+Rails.application.config.assets.precompile += %w( img/bg/1.jpg )
+Rails.application.config.assets.precompile += %w( custom.css )
+
 Rails.application.config.assets.precompile += %w( css/app.min.css )
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
 
-Rails.application.config.assets.precompile += %w( driver_profile_show.js )
-Rails.application.config.assets.precompile += %w( vehicle_location_show.js )
+
 
