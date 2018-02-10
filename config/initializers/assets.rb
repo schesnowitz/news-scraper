226818e62df93009af44a4284250a8d45a0dc77c
Steve Chesnowitz <steve@chesnowitz.com>
2018-02-10 11:09:46 -0500

    not working production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,9 @@ Rails.application.config.assets.precompile += %w( material-design-iconic-font/di
 Rails.application.config.assets.precompile += %w( animate.css/animate.min.css )
 Rails.application.config.assets.precompile += %w( jquery.scrollbar/jquery.scrollbar.css )
 Rails.application.config.assets.precompile += %w( fullcalendar/dist/fullcalendar.min.css )
-Rails.application.config.assets.precompile += %w( img/bg/1.jpg )
+Rails.application.config.assets.precompile += %w( 1.jpg )
 Rails.application.config.assets.precompile += %w( custom.css )
-
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.precompile += %w( css/app.min.css )
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
