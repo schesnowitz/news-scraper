f59b549c0a8104b70ebec3154e4b7913fad24818
Steve Chesnowitz <steve@chesnowitz.com>
2018-02-10 13:31:21 -0500

    deploy saturday

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.active_record.dump_schema_after_migration = false
+
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:                  'smtp.gmail.com',
